8b671f907ff511847adb814928ab59cf169cd317
MFadhlanHkim <113012448+MFadhlanHkim@users.noreply.github.com>
2024-04-14 01:58:48 +0700

    Add files via upload

---
+++ Repo1/Fadhlan.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+df = pd.read_csv("Salary_Data.csv")
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[4]:
+
+
+df.info()
+
